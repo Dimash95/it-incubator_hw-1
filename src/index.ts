09c9e399fef8974acff31272f316927cff8b1f48
Dimash <dinmukhamed.amirov@gmail.com>
2025-11-07 15:53:21 +0800

    feat: do post

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ const express = require("express");
 import { Request, Response } from "express";
 import data from "./data.json";
 
+type PostVideo = {
+  title: string;
+  author: string;
+  availableResolutions: string[];
+};
+
 const app = express();
 const port = 3000;
 
@@ -11,7 +17,18 @@ app.get("/", (req: Request, res: Response) => {
   res.send(data);
 });
 
-app.get("/", (req: Request, res: Response) => {
+app.post("/", (req: Request, res: Response) => {
+  const { title, author, availableResolutions } = req.body;
+  data.push({
+    id: data.length,
+    title,
+    author,
+    canBeDownloaded: true,
+    minAgeRestriction: null,
+    createdAt: new Date().toISOString(),
+    publicationDate: new Date().toISOString(),
+    availableResolutions,
+  });
   res.send(data);
 });
 
