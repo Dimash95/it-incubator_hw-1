01192e5656f89a48447f4988f149188324b7c89c
Dimash <dinmukhamed.amirov@gmail.com>
2025-11-08 16:19:34 +0800

    fix: vercel serverless handler

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 import express, { Request, Response } from "express";
 // import { apiRouter } from "./router";
-import serverless from "serverless-http";
+// import serverless from "serverless-http";
 
 const app = express();
-// const port = 5000;
+const port = 5000;
 
 app.use(express.json());
 // app.use("/api", apiRouter);
 app.get("/", (req: Request, res: Response) => res.send("API is working ✅"));
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 export default app;
 
-export const handler = serverless(app);
+// export const handler = serverless(app);
